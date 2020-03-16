3dcc216a9679c1f14cb1649d4cd42fc4418e06f0
manunshirur <manunshirur@gmail.com>
2020-03-16 16:34:01 -0500

    Date Range Change in Server Query

--- Server/demo/src/main/java/com/example/services/DAO.java
+++ Server/demo/src/main/java/com/example/services/DAO.java
@@ -30,7 +30,7 @@ public class DAO {
         		"	SELECT id as date_id, date_yyyy_mm_dd  " + 
         		"	from master_dates  " + 
         		"    where  " + 
-        		"    id >= REPLACE(DATE_SUB(date(now()), INTERVAL 60 DAY),'-','') " +
+        		"    id >= REPLACE(DATE_SUB(date(now()), INTERVAL 70 DAY),'-','') " +
         		"    and  " + 
         		"    id <=  REPLACE(DATE_SUB(date(now()), INTERVAL 45 DAY),'-','')  " +
         		"    order by id " + 
